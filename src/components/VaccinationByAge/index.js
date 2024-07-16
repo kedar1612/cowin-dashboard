34331b52b13fadc2ca32e7351e964ab4c5fc54f9
kedar gaikwad <kedarga1@gmail.com>
2024-07-16 12:13:59 +0000

    error

---
+++ src/components/VaccinationByAge/index.js
@@ -0,0 +1,34 @@
+// Write your code here
+import './index.css'
+
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+
+const VaccinationByAge = props => {
+  const {vaccinationByAge} = props
+
+  return (
+    <div className="VaccinationByAge">
+      <h1 className="heading">Vaccination by Age</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={vaccinationByAge}
+            startAngle={0}
+            endAngle={360}
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="18-44" fill="#2d87bb" />
+            <Cell name="44-60" fill="#a3df9f" />
+            <Cell name="Above 60" fill="#64c2a6" />
+          </Pie>
+          <Legend iconType="circle" verticalAlign="middle" align="center" />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default VaccinationByAge
